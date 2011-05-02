183e67b9b61c1c94ddf46ee6f02515ac5fbd244c
Christian Bunk <christian.bunk@student.HTW-Berlin.de>
2011-05-02 21:32:09 +0200

    write output in file

--- src/index.cpp
+++ src/index.cpp
@@ -22,17 +22,30 @@ void Index::createIndex(string *outputfile, string *inputfile) {
 	ifstream in(inputfile->c_str());
 	istream_iterator<string> pos(in), end ;
 	
-	if(pos == end) {
+	if (!in) {
 		cout << "Eingabedatei nicht gefunden!" << endl;
 		return;
 	};
 	
+	// kopiere Wörter in vector wort
 	copy(pos, end, back_inserter(*wort)) ;
 	
 	in.close() ; // Datei schließen
 	
+	// kopiere in wortlist, eigentlich überflüssig...
 	copy(wort->begin(), wort->end(), back_inserter(*wortlist)) ;
 	
 	copy(wortlist->begin(), wortlist->end(), 
 		ostream_iterator<string>(cout, "\n")) ;
+		
+	// kopiere wörter in output datei
+	ofstream out(outputfile->c_str()); 
+	if (!out) { 
+		cerr << "Datei " << outputfile << " kann nicht geoeffnet werden." << endl; 
+	} else {
+		copy(wortlist->begin(), wortlist->end(), 
+			ostream_iterator<string>(out, "\n")) ;
+	}
+	
+	out.close();
 }
