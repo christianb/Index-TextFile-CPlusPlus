21a775046a67c826086f24aaa6e3b6a3e92f22d3
Christian Bunk <christianb.github@googlemail.com>
2011-05-15 11:41:15 +0200

    add some comments

--- src/lexic.cpp
+++ src/lexic.cpp
@@ -13,38 +13,41 @@ bool Lexic::operator()(string s1, string s2) {
 		const char c1 = *s1_it;
 		const char c2 = *s2_it;
 			
+		// if the character c1 is lower than c2
 		if (tolower(c1) < tolower(c2)) {
 			//cout << c1 << " ist kleiner als " << c2 << endl;
-			s1_IsSmaller_s2 = true; // fertig
+			s1_IsSmaller_s2 = true; // the string s1 is lexicografically lower than s2, done
 			loop_flag = false;
 		}
 			
+		// if the character c1 is grater than c2
 		if (tolower(c1) > tolower(c2)) {
 			//cout << c1 << " ist groesser als " << c2 << endl;
-			s1_IsSmaller_s2 = false; // fertig
+			s1_IsSmaller_s2 = false; // the string s1 is lexicografically grater than s2, done
 			loop_flag = false;
 		}
 				
+		// if both characters are the same
 		if (tolower(c1) == tolower(c2)) {
 			//cout << c1 << " ist gleich " << c2 << endl;
 			loop_flag = true;
 					
-			s1_it++;
+			s1_it++; // go to next character
 			s2_it++;
 					
 			// check if both strings are emtpy
 			if (s1_it == s1.end() && s2_it == s2.end()) {
-				loop_flag = false;
+				loop_flag = false; // done
 			} else {
 				// if one of both is empty, than the shorter string is smaller
 				if (s1_it == s1.end()) {
-					// if s1 is empty
+					// if s1 is empty, s1 is lexicorafically smaller than s2
 					s1_IsSmaller_s2 = true;
 					loop_flag = false;
 				}
 
 				if (s2_it == s2.end()) {
-					// if s2 is empty
+					// if s2 is empty, s1 is lexicorafically grater than s2
 					s1_IsSmaller_s2 = false;
 					loop_flag = false;
 				}
