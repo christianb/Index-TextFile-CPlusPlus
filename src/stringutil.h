8b3afcf9bc12e1d8967385f73fa15e7e595e3c40
Christian Bunk <christianb.github@googlemail.com>
2011-05-17 21:20:59 +0200

    comment class StringUtil

--- src/stringutil.h
+++ src/stringutil.h
@@ -1,3 +1,13 @@
+#ifndef _STRINGUTIL_H
+#define _STRINGUTIL_H
+
+#include <string>
+//#include <iostream>
+#include <sstream>
+//#include <algorithm>
+
+using namespace std;
+
 /** Class StringUtil
  *
  *  This class provides some usefull string methods.
@@ -6,22 +16,12 @@
  *  date    15.5.2011
  *  version 0.1
  */
-
-#ifndef _STRINGUTIL_H
-#define _STRINGUTIL_H
-
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <algorithm>
-
-using namespace std;
-
 class StringUtil {
 public:
+	
 	/**
 	 * Convert a single value into a string.
-	 * @param value The value to be convert into a string.
+	 * @param value The value to be convert into a string. Must implement operator<<() !
 	 * @return string The string representation of the value.
 	 */
 	template <typename Type>
@@ -33,21 +33,21 @@ public:
 	}
 	
 	/**
-	 * Convert a string into a given data type (call-by-reference)
+	 * Convert a string into a given data type
 	 * @param str The string to be converted into a special data type.
-	 * @param &wert The variable where the representation of the string definded by the data type is stored.
+	 * @param &value The variable where the representation of the string definded by the data type is stored. call-by-reference! Must implement operator>>() !
 	 */
-	template <typename Typ>
-	void stringTo(string str, Typ &wert) {
-	    istringstream strin; // Unser Eingabe-Stream
+	template <typename Type>
+	void stringTo(string str, Type &value) {
+	    istringstream strin;
 
-	    strin.str(str);           // Streaminhalt mit String-Variable füllen
-	    strin >> wert;            // Variable von Eingabe-Stream einlesen
+	    strin.str(str);
+	    strin >> value;
 	}
 	
 	/**
 	 * Transfomrs a given string to lower case letters.
-	 * @param &str The string to be transformed given as an reference.
+	 * @param &str The string to be transformed given as an reference. call-by-reference!
 	 */
 	void toLowerCase(string &str);
 	
