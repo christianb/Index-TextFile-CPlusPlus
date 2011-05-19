756c6c4590e2e214c0c1163a10ffab171dae18d2
Christian Bunk <christianb.github@googlemail.com>
2011-05-19 11:25:39 +0200

    changes on lexic compare, but needs test on ubuntu

--- src/lexic.cpp
+++ src/lexic.cpp
@@ -1,7 +1,43 @@
 #include "Lexic.h"
 
+using namespace std;
+
 bool Lexic::operator()(string s1, string s2) const {
-	return lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(), mycomp);
+	//if (!lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(), mycomp)) {
+		string::iterator s1_it = s1.begin();
+		string::iterator s2_it = s2.begin();
+		
+		// unix
+		if  (strcasecmp(s1.c_str(), s2.c_str()) < 0) {
+			return true;
+		}
+		
+		if  (strcasecmp(s1.c_str(), s2.c_str()) > 0) {
+			return false;
+		}
+		
+		if (strcasecmp(s1.c_str(), s2.c_str()) == 0) {
+			// if equal lexic test of asci difference
+			do {
+				if (*s1_it < *s2_it) {
+					return true;
+				}
+				
+				if (*s1_it > *s2_it) {
+					return false;
+				}
+				
+				s1_it++;
+				s2_it++;
+			} while(s1_it != s1.end() || s2_it != s2.end());
+		}
+		
+		
+		return false;
+	//}
+	
+	//return lexicographical_compare(s1.begin(), s1.end(), s2.begin(), s2.end(), mycomp);
+	//return true;
 }
 
 // a case-insensitive comparison function:
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 
 #include "controller.h"
+
+using namespace std;
 
 int main (int argc, char *argv[]) {
 	Controller *ctr = new Controller(argc, argv);
